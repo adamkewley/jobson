4f35948b24f92e19674a33a0f47ed34cc9499ce5
Adam Kewley <contact@adamkewley.com>
2019-06-16 11:35:30 +0100

    Updated demo script with something a little more interactive-looking

--- jobson/src/main/resources/demo-script.sh
+++ jobson/src/main/resources/demo-script.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 echo "Jobson can run *any* application. This demo is launching a bash script"
+sleep 1
 echo ""
 echo "At runtime, Jobson created a clean working directory at $(pwd)"
 echo "Arguments and dependencies get copied to that directory"
 echo ""
+
 echo "Jobson captures stdout"
+sleep 3
 echo "Jobson also captures stderr" 1>&2
+sleep 3
+
 echo "And provides support for capturing user inputs (as expectedInputs)"
 echo "The inputs can be routed to the actual application (e.g. this script)"
 echo ""
@@ -22,5 +27,16 @@ echo "Jobson copies output files to persistence after the application finishes."
 echo "I didn't want to risk a library problem, so this output is just copied from the dependencies"
 mv demo-dependency output
 
+sleep 2
+echo "The output has been 'produced' by the demo script, after this script exits, Jobson will copy it to its file storage"
 
-
+echo "Execution finishes in 5..."
+sleep 1
+echo "4..."
+sleep 1
+echo "3..."
+sleep 1
+echo "2..."
+sleep 1
+echo "1..."
+sleep 1
